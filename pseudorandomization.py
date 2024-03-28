39c127a0ac0995a6e0b16a5a71f6e880975b73dc
berfinbastug <berfin.bastug@gmail.com>
2024-03-28 13:53:27 +0100

    create pseudorandomized tables beforehand

--- pseudorandomization.py
+++ pseudorandomization.py
@@ -1,14 +1,30 @@
-    #=====================
-    #PSEUDORANDOMIZATION
-    #=====================
-    # condition: any value should not occur more than three times consecutively
-# shuffle dataframe rows until the condition is met
-while True:
-    df_shuffled = table_values.sample(frac=1).reset_index(drop=True)  # Shuffle rows
-    if check_consecutive_occurrences(df_shuffled['percentage']):
-        break
+from data_frame_functions import check_consecutive_occurrences
+import experiment_params as params
+import os
+import pandas as pd
 
-# Save the shuffled DataFrame as a TSV file
+table_dir = params.table_dir
+files = os.listdir(table_dir)
+# Filter out only the files with the .tsv extension
+tsv_files = [file for file in files if file.endswith('.tsv')]
 
-table_path = os.path.join(table_dir, tsv_filename)
-df_shuffled.to_csv(table_path, sep='\t', index=False)
+for itable in tsv_files:
+    df_path = os.path.join(table_dir, itable)
+    df = pd.read_csv(df_path, delimiter='\t')
+    
+    for i in range(20):
+        pid = i + 1
+        #=====================
+        #PSEUDORANDOMIZATION
+        #=====================
+        # condition: any value should not occur more than three times consecutively
+        # shuffle dataframe rows until the condition is met
+        while True:
+            df_shuffled = df.sample(frac=1).reset_index(drop=True)  # Shuffle rows
+            if check_consecutive_occurrences(df_shuffled['percentage']):
+                break
+
+        # Save the shuffled DataFrame as a TSV file
+        table_name = 'participantid_' + str(pid) + '_randomized_' + itable
+        table_path = os.path.join(table_dir, table_name)
+        df_shuffled.to_csv(table_path, sep='\t', index=False)
