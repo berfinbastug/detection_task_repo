477c9633483b80c23b6b2730788e6dcc1a6c99a5
berfinbastug <berfin.bastug@gmail.com>
2024-03-28 12:54:19 +0100

    some more changes

--- stim_production_functions.py
+++ stim_production_functions.py
@@ -5,7 +5,6 @@
 #=====================
 
 import numpy as np
-import itertools
 import stimulus_params
 import copy
 
@@ -159,42 +158,5 @@ def gencloudcoherence(sP=deepCopysP, change_dict = None):
     y = np.concatenate((zpad, x, zpad), axis=0)
 
     return y, sP
-
-
-def generate_baseline_table(unit_dur_list, rep_percentage_list):
-    # first, generate bunch of lists
-    table_values = []
-
-    for iunitdur, ipercent in itertools.product(range(len(unit_dur_list)), range(len(rep_percentage_list))):
-        
-        unitdur = unit_dur_list[iunitdur]
-        percentage = rep_percentage_list[ipercent]
-        stim_code = (iunitdur + 1) * 100 + (ipercent + 1)
-
-        num_examples = 90 if percentage == 0 else 10
-        expected_response = 0 if percentage == 0 else 1
-
-        for _ in range(num_examples):
-            table_values.append([unitdur, percentage, stim_code, expected_response])
-
-        ntrials = len(table_values)
-
-    return table_values, ntrials
-
-
-#=====================
-#PSEUDORANDOMIZATION
-#=====================
-# Function to check if any value occurs more than three times consecutively
-def check_consecutive_occurrences(arr):
-    count = 1
-    for i in range(1, len(arr)):
-        if arr[i] == arr[i-1]:
-            count += 1
-            if count > 3:
-                return False
-        else:
-            count = 1
-    return True
 
 
