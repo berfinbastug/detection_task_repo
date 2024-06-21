10022e0352c5c283200624ec763b5d54841666fc
berfinbastug <berfin.bastug@gmail.com>
2024-06-21 11:54:12 +0200

    more changes

--- pseudorandomization.py
+++ pseudorandomization.py
@@ -1,13 +1,22 @@
-from data_frame_functions import check_consecutive_occurrences
-import experiment_params as params
 import os
-import pandas as pd
+
+# Function to check if any value occurs more than three times consecutively
+def check_consecutive_occurrences(arr):
+    count = 1
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i-1]:
+            count += 1
+            if count > 3:
+                return False
+        else:
+            count = 1
+    return True
+
 
 def shuffled_df(df, table_dir, participant_id, which_block):
 
     #PSEUDORANDOMIZATION
-    #=====================
-    # condition: any value should not occur more than three times consecutively
+    # condition: any value (percentage) should not occur more than three times consecutively
     # shuffle dataframe rows until the condition is met
     while True:
         df_shuffled = df.sample(frac=1).reset_index(drop=True)  # Shuffle rows
@@ -15,7 +24,7 @@ def shuffled_df(df, table_dir, participant_id, which_block):
             break
 
     # Save the shuffled DataFrame as a TSV file
-    table_name = 'participant_id_'+ str(participant_id) + '_randomized_block_' + str(which_block) + '_table.tsv'
+    table_name = 'detection_experiment_participant_id_'+ str(participant_id) + '_randomized_block_' + str(which_block) + '_table.tsv'
     #print(table_name)
     table_path = os.path.join(table_dir, table_name)
     #print(table_path)
