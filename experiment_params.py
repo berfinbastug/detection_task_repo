eb232b99a1f09fc826172841685a5735bd226270
berfinbastug <berfin.bastug@gmail.com>
2024-06-21 10:16:25 +0200

    preparation for the experimental sessions

--- experiment_params.py
+++ experiment_params.py
@@ -3,9 +3,10 @@
 #=====================
 # DURATION, IV 1
 # independent variable
-# Leo participated in the pilot experiment in 27.03.2024. In his version, I was using one duration condition. He told me that
-# he used the rhythm induced by the duration as a template to detect repetition. To prevent this, I am adding other
-# duration conditions to my detection experiment right now (28.03.2024)
+# Leo participated in the pilot experiment in 27.03.2024. 
+# In his version, I was using one duration condition. He told me that
+# he used the rhythm induced by the duration as a template to detect repetition. 
+# To prevent this, I am adding other duration conditions to my detection experiment right now (28.03.2024)
 min_unit_dur = 0.4  
 max_unit_dur = 1
 n_unit_dur_cond = 3
@@ -17,10 +18,13 @@ n_rep_percentage_cond = 10  # define the number of elements
 
 nblocks = 3
 iti = 1  # inter trial interval
+
 #=====================
 #OUTPUT
 #=====================
+# planning to change this!
 keys_output_columns = ['block_idx','trial_idx', 'rt','key_name', 'key_tDown', 'button_press_duration','stim_code', 'expected_response', 'actual_response', 'counterbalance_condition']
+
 #=====================
 #SET UP AUDIO PARAMETERS
 #=====================
